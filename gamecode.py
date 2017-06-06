be35d5e6c0cf3191aa9010bbf268c0da757bd7b0
999010alex <alexandercastillo067@gmail.com>
2017-06-06 11:01:12 -0400

    Update gamecode.py

--- gamecode.py
+++ gamecode.py
@@ -9,7 +9,7 @@ you=20
 terrorists=0
 hunger=100
 thirst=100
-ingredient = print("Pepperoni")
+ingredient = print("Chicken")
 def mix_and_cook():
     print("Flatten out dough")
     print("put tomato sauce on dough")
@@ -22,7 +22,32 @@ def make_pizza(ingredient):
     pizza = 'a {} pizza!'.format(ingredient)
     return pizza
 def mix_and_cook2():
-    print("
+    print("Grill meat and cheese")
+    print("Put meat and cheese in taco shell")
+    print("Put rice and beans in taco shell")
+    print("put lettuce and tomatoes in taco shell")
+    print("put sour cream and guacamole in taco shell")
+def make_taco(ingredient):
+    print("How to make a taco!")
+    mix_and_cook2()
+    taco = 'a {} taco!'.format(ingredient)
+def mix_and_cook3():
+    print("Put mustard or mayo and cheese on bread")
+    print("Put meat on bread")
+    print("toast sandwhich")
+    print("put lettuce and tomatoes on bread")
+def make_sandwich(ingredient):
+    print("How to make a sandwich!")
+    mix_and_cook3()
+    sandwich = 'a {} sandwich!'.format(ingredient)
+def mix_and_cook4():
+    print("take hot pocket out of package")
+    print("put hot pocket in conviently nearby microwave")
+    print("cook for 3 minutes")
+def make_hot_pocket(ingredient):
+    print("How to make a hot pocket!")
+    mix_and_cook4()
+    hot_pocket = 'a {} hot pocket!'.format(ingredient)
 if done == True:
     print("You have finished the game, ethier you've died, or you made it out of Herozitovia. Restart to play again.")
 while done == False:
@@ -59,8 +84,8 @@ while done == False:
     elif choice == "b" or choice == "B":
         print("You drank water, your thirst goes down but the natives are getting closer.")
         you=you+5
-        natives=natives+23
-        camel=camel+5
+        terrorists=terrorists+23
+        hunger=hunger+5
         thirst=thirst+20
     elif choice == "c" or choice == "C":
         print("Which meal shall you eat?")
@@ -71,39 +96,43 @@ while done == False:
         choice=input('Your choice: ')
         if choice == "A" or choice == "a":
             print("You unbox to pizza to find it uncooked, luckily, there happens to be a pizza oven nearby.")
-            
+            print(make_pizza)
             you=you+25
             terrorists=terrorists+18
-            hunger=hunger-20
+            hunger=hunger+25
             thirst=thirst-20
 
         elif choice == "b" or choice == "B":
-            print("You drank water, your thirst goes down but the terrorists are getting closer.")
+            print(make_taco)
             you=you+5
             terrorists=terrorists+23
-            hunger=hunger+5
+            hunger=hunger+20
             thirst=thirst+20
         elif choice == "c" or choice == "C":
             print("Which meal shall you eat?")
+            print(make_sandwich)
             you=you+1
             terrorists=terrorists+20
-            hunger=hunger+25
+            hunger=hunger+15
             thirst=thirst+5
         elif choice == "d" or choice == "D":
+            print(make_hot_pocket)
             you=you+1
             terrorists=terrorists+20
-            hunger=hunger+25
+            hunger=hunger+10
             thirst=thirst+5
+        else:
+            print("you have a wrong input")
     elif choice == "d" or choice == "D":
-        print("You ride 15 miles, but you get more thirsty, and your camel loses energy.")
+        print("You run 15 miles, but you get more thirsty, and you get hungrier.")
         you=you+15
-        natives=natives+20
-        camel=camel-15
+        terrorists=terrorists+20
+        hunger=hunger-15
         thirst=thirst-20
     elif choice == "e" or choice == "E":
         print("You are ", you ," miles out of 200 miles.")
-        print("The natives are ", you-natives , "miles away from you")
-        print("The camel has ", camel ,"energy out of 100.")
+        print("The terrorists are ", you-terrorists , "miles away from you")
+        print("Your hunger level is ", hunger ,"energy out of 100.")
         print("You are ", thirst, " thirst out of 100.")
     else:
         print("You have a wrong input")
